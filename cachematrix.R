6304d8a4784ba63f77025cde5b2685a224348553
raho73 <ralf.hoenke@web.de>
2017-07-22 22:11:26 +0200

    what a pain

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,29 @@
-## Put comments here that give an overall description of what your
-## functions do
+makeCacheMatrix <- function(x = matrix()) 
+{
+	invMA 	<- NULL 													#def. inverted matrix NULL
+	setMA 	<- function(y) 												#set matrix
+		{											
+            x <<- y
+            invMA <<- NULL
+		}
+	getMA 	<- function() x												#get matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+	setinv 	<- function(inverse) invMA <<- inverse 						#set inv matrix
+    getinv 	<- function() invMA 										#get inv matrix
+	list(set=setMA, get=getMA,CAsetinv=setinv,CAgetinv=getinv)  		#for later access
 }
 
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve 	<- function(x, ...) 
+{
+    invMA	<-x$CAgetinv()												#try loading inv matrix from cache
+	if(!is.null(invMA))													#if cache is there return it and leave function
+		{
+            message("get inv matrix from cache")
+            return(invMA)
+		}
+			message("calculation required")
+	newMA 	<-x$get()													#calc inv matrix
+	invMA	<-solve(newMA,...)
+	x$CAsetinv(invMA)													#cache inv matrix
+	invMA
 }
