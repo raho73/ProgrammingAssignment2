f99366e272b244b977b42e2245160c64b59fb5e1
raho73 <ralf.hoenke@web.de>
2017-07-24 09:12:27 +0200

    format in 1st version was destroyed

--- cachematrix.R
+++ cachematrix.R
@@ -1,29 +1,52 @@
+##### makeCacheMatrix function ###############################
 makeCacheMatrix <- function(x = matrix()) 
 {
-	invMA 	<- NULL 													#def. inverted matrix NULL
-	setMA 	<- function(y) 												#set matrix
+  #def. inverted matrix NULL
+  invMA <- NULL
+  
+  #set matrix
+	setMA<- function(y) 											
 		{											
-            x <<- y
-            invMA <<- NULL
+      x<<- y
+      invMA <<- NULL
 		}
-	getMA 	<- function() x												#get matrix
-
-	setinv 	<- function(inverse) invMA <<- inverse 						#set inv matrix
-    getinv 	<- function() invMA 										#get inv matrix
-	list(set=setMA, get=getMA,CAsetinv=setinv,CAgetinv=getinv)  		#for later access
+	
+	#get matrix
+	getMA<- function() x												
+	
+	#set inv matrix
+	setinv<- function(inverse) invMA <<- inverse
+	
+	#get inv matrix
+  getinv<- function() invMA
+  
+  #for later access
+	list(set=setMA, get=getMA,CAsetinv=setinv,CAgetinv=getinv)  		
 }
+##### end makeCacheMatrix ####################################
 
+##### cacheSolve function ####################################
 cacheSolve 	<- function(x, ...) 
 {
-    invMA	<-x$CAgetinv()												#try loading inv matrix from cache
-	if(!is.null(invMA))													#if cache is there return it and leave function
+  #try loading inv matrix from cache 
+  invMA<-x$CAgetinv()
+  
+  #if cache is there return it and leave function
+	if(!is.null(invMA))													
 		{
-            message("get inv matrix from cache")
-            return(invMA)
+      message("get inv matrix from cache")
+      return(invMA)
 		}
-			message("calculation required")
-	newMA 	<-x$get()													#calc inv matrix
-	invMA	<-solve(newMA,...)
-	x$CAsetinv(invMA)													#cache inv matrix
+	message("calculation required")
+	 
+	#calc inv matrix
+	newMA<-x$get()													
+	invMA<-solve(newMA,...)
+	
+	#cache inv matrix
+	x$CAsetinv(invMA)
+	
+	#return inv matrix
 	invMA
 }
+##### end cacheSolve #########################################
